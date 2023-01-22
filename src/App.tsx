525ef18148bc5d8243744acdf883eb6dd3bc9b44
Tomas Kukis <tomas@kukis.net>
2023-01-22 11:27:38 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Dropdown } from "./components/Dropdown";
+
+const App = () => {
+  return (
+    <div>
+      <Dropdown />
+    </div>
+  );
+}
+
+export default App;
